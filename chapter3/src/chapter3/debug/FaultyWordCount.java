4589ff91f6724f20e78dcd378dab70c7f1f2f90e
thilg <csethil@gmail.com>
2014-02-10 14:52:37 -0500

    refactoring

--- chapter3/src/chapter3/debug/FaultyWordCount.java
+++ chapter3/src/chapter3/debug/FaultyWordCount.java
@@ -17,7 +17,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
